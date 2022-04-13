d3816ef2264683b423991c7fc6756b428f2d883d
k.khoruzhenko <khoruzhenko.work@gmail.com>
2022-04-13 23:55:31 +0300

    conflicts fix

--- reactNative/reactNativeDesktop/App.tsx
+++ reactNative/reactNativeDesktop/App.tsx
@@ -8,12 +8,17 @@
  * @format
  */
 
-import React from 'react';
 // import {useTranslation} from 'react-i18next';
+import React, {useCallback, useEffect, useState} from 'react';
 import {
+  Appearance,
+  Button,
+  ColorSchemeName,
+  Pressable,
   SafeAreaView,
   ScrollView,
   StyleSheet,
+  Switch,
   Text,
   useColorScheme,
   View,
@@ -21,15 +26,66 @@ import {
 
 import {
   Colors,
-  Header,
   LearnMoreLinks,
 } from 'react-native/Libraries/NewAppScreen';
 
+const Section: React.FC<{
+  title: string;
+  isDarkMode: boolean;
+  children: any;
+}> = ({children, title, isDarkMode}) => {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState<ColorSchemeName>();
+  const isDarkMode = theme === 'dark';
+
+  const themeChangeListener = useCallback(() => {
+    setTheme(Appearance.getColorScheme());
+  }, []);
+
+  useEffect(() => {
+    const list = Appearance.addChangeListener(themeChangeListener);
+    return () => list.remove();
+  }, [themeChangeListener]);
+
+  const changeTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: isDarkMode ? Colors.darker : Colors.light,
+  };
+
+  const handleCountIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleCountReset = () => {
+    setCount(0);
   };
 
   // const App = () => (
@@ -56,11 +112,43 @@ const App = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
+        {/* <Header /> */}
+        <View style={[styles.rootViewContainer, backgroundStyle]}>
+          <Switch
+            trackColor={{false: '#767577', true: '#81b0ff'}}
+            // thumbColor={theme === 'light' ? '#f5dd4b' : '#f4f3f4'}
+            // ios_backgroundColor="#3e3e3e"
+            onValueChange={changeTheme}
+            value={theme === 'light'}
+          />
+          <Section title="Increament example" isDarkMode={theme === 'dark'}>
+            Counter: <Text style={styles.highlight}>{count}</Text>
+          </Section>
+          <View style={styles.buttonCounterContainer}>
+            <Pressable
+              style={[
+                styles.buttonCountIncr,
+                backgroundStyle,
+                {
+                  borderColor: isDarkMode ? Colors.white : Colors.black,
+                },
+              ]}
+              onPress={handleCountIncrement}>
+              <Text
+                style={[
+                  {
+                    color: isDarkMode ? Colors.white : Colors.black,
+                  },
+                ]}>
+                Count++
+              </Text>
+            </Pressable>
+            <Button
+              title="Reset"
+              // color={isDarkMode ? Colors.light : Colors.dark}
+              onPress={handleCountReset}
+            />
+          </View>
           <LearnMoreLinks />
         </View>
       </ScrollView>
@@ -69,7 +157,11 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
+  rootViewContainer: {
+    alignItems: 'center',
+  },
   sectionContainer: {
+    width: 500,
     marginTop: 32,
     paddingHorizontal: 24,
   },
@@ -84,6 +176,23 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  buttonCounterContainer: {
+    marginHorizontal: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 5,
+    cursor: 'pointer',
+  },
+  buttonCountIncr: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    borderWidth: 1,
   },
 });
 
